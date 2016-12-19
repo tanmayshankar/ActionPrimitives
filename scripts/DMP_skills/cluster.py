2b068507c3217984780f4ce6ed14ef36c9b184a0
tanmayshankar <tanmay.shankar@gmail.com>
2016-12-18 22:52:10 -0500

    Updating files and adding data files generated and clustered.

--- scripts/DMP_skills/cluster.py
+++ scripts/DMP_skills/cluster.py
@@ -4,7 +4,7 @@ from DMP import *
 from DMP_Library import *
 import sklearn.manifold as skl_manifold
 
-number_trajectories = 9
+number_trajectories = 20
 number_segments = 9
 number_samples = number_segments*number_trajectories
 number_kernels = 20
@@ -14,7 +14,7 @@ number_dimensions = 2
 weights = npy.load(str(sys.argv[1]))
 weights_2d = weights.reshape((weights.shape[0]*weights.shape[1],weights.shape[2]))
 
-number_clusters = 10
+number_clusters = 20
 kmeans = KMeans(n_clusters = number_clusters, random_state=0).fit(weights.reshape(number_samples,number_kernels*number_dimensions))
 
 cluster_labels = kmeans.labels_
@@ -28,37 +28,48 @@ point_labels = npy.zeros((number_samples, number_kernels))
 
 for i in range(number_trajectories):
 	for j in range(number_segments):
-		point_labels[number_trajectories*i+j,:] = i
-		labels[number_trajectories*i+j,:] = kmeans.labels_[number_trajectories*i+j]
+		# point_labels[number_trajectories*i+j,:] = i
+		point_labels[number_segments*i+j,:] = i
+		# labels[number_trajectories*i+j,:] = kmeans.labels_[number_trajectories*i+j]
+		labels[number_segments*i+j,:] = kmeans.labels_[number_segments*i+j]
 
 point_labels = point_labels.reshape(number_samples*number_kernels,1)
 labels = labels.reshape(number_samples*number_kernels,1)
 
 model = skl_manifold.TSNE(n_components=2,random_state=0)
-embedded_weights = model.fit_transform(weights.reshape(81,40))
+embedded_weights = model.fit_transform(weights.reshape(number_samples,40))
 
-plt.scatter(points_2d[:,0],points_2d[:,1],c=point_labels,s=400)
+plt.scatter(points_2d[:,0],points_2d[:,1],c=point_labels,s=200)
 plt.title('Plot data points of each trajectory.')
+plt.colorbar()
 plt.savefig('Original_Trajectories.png',bbox_inches='tight')
+plt.close()
 # plt.show()
 
-plt.scatter(points_2d[:,0],points_2d[:,1],c=labels,s=400)
+plt.scatter(points_2d[:,0],points_2d[:,1],c=labels,s=200)
 plt.title('Plot of clustered data points.')
+plt.colorbar()
 plt.savefig('Clustered_Trajectories.png',bbox_inches='tight')
+plt.close()
 # plt.show()
 
-plt.scatter(weights_2d[:,0],weights_2d[:,1],c=labels,s=400)
+plt.scatter(weights_2d[:,0],weights_2d[:,1],c=labels,s=200)
 plt.title('Plot clustered weights of DMPs.')
+plt.colorbar()
 plt.savefig('Clustered_Weights.png',bbox_inches='tight')
+plt.close()
 # plt.show()
 
-plt.scatter(weights_2d[:,0],weights_2d[:,1],c=point_labels,s=400)
+plt.scatter(weights_2d[:,0],weights_2d[:,1],c=point_labels,s=200)
 plt.title('Plot of Weights colored by Trajectory.')
+plt.colorbar()
 plt.savefig('Trajectory_Weights.png',bbox_inches='tight')
+plt.close()
 # plt.show()
 
-plt.scatter(embedded_weights[:,0],embedded_weights[:,1],c=kmeans.labels_,s=400)
+plt.scatter(embedded_weights[:,0],embedded_weights[:,1],c=kmeans.labels_,s=200)
 plt.title('Plot Embedded Weights using TSNE.')
 plt.savefig('Embedded_Weights.png')
+plt.close()
 # plt.show()
 
