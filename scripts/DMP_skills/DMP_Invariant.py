a2ece7803b0472d4669146c4a35bb5bdfeb4b491
tanmayshankar <tanmay.shankar@gmail.com>
2016-12-19 14:40:18 -0500

    Updating Implementation of Auke / Schaal's DMPs.

--- scripts/DMP_skills/DMP_Invariant.py
+++ scripts/DMP_skills/DMP_Invariant.py
@@ -26,7 +26,6 @@ class DMP:
 		self.gaussian_kernels[:,0] = 0.1
 		# Setting mean. 
 		self.gaussian_kernels[:,1] = npy.array(range(self.number_kernels)).astype(float)/self.number_kernels
-
 		self.weights = npy.zeros((self.number_kernels,self.dim))
 
 		# Assuming 2D data.
@@ -34,14 +33,10 @@ class DMP:
 		self.vel = npy.zeros((self.T,self.dim))
 		self.acc = npy.zeros((self.T,self.dim))
 
-		self.phi = npy.zeros((self.T,self.number_kernels))
+		# self.phi = npy.zeros((self.T,self.number_kernels))
 		self.target_forces = npy.zeros((self.T,self.dim))
-
-	# def load_pos(self):
-	# 	self.pos = npy.loadtxt(str(sys.argv[1]))
-	# 	self.vel = npy.diff(self.pos,axis=0)
-	# 	self.acc = npy.diff(self.vel,axis=0)
-	# 	self.T = self.pos.shape[0]
+		self.eta = npy.zeros((self.T, self.dim))
+		self.phi = npy.zeros((self.number_kernels,self.T, self.T))
 
 	def load_pos(self, pose):
 		self.pos = copy.deepcopy(pose)
@@ -62,42 +57,44 @@ class DMP:
 		self.theta = npy.exp(-self.alpha*float(time)/self.T)
 		return self.theta
 
- 	def calc_target_force_vanilla(self,time):
-		return (1./(self.pos[self.T-1]-self.pos[0]))*(-self.K*self.pos[time]+self.D*self.vel[min(time,self.vel.shape[0]-1)]+self.T*self.acc[min(time,self.acc.shape[0]-1)])    
-
-	def calc_target_force_pastor(self,time):
-		return -(self.pos[self.T-1]-self.pos[time])+(self.pos[self.T-1]-self.pos[0])*self.calc_phase(time)+(self.T*self.acc[min(time,self.acc.shape[0]-1)]-self.D*self.vel[min(time,self.vel.shape[0]-1)])/self.K
-
 	def calc_target_force_auke(self,time):
 		return (self.T**2)*self.acc[min(time,self.acc.shape[0]-1)] - self.alphaz*(self.betaz*(self.pos[self.T-1]-self.pos[time])-self.T*self.vel[min(time,self.vel.shape[0]-1)])
 
-	def update_target_force_vanilla(self):
-		for t in range(0,self.T):
-			self.target_forces[t,:]=self.calc_target_force_vanilla(t)
-
 	def update_target_force_auke(self):
 		for t in range(0,self.T):
-			self.target_forces[t,:]=self.calc_target_force_auke(t)/(self.pos[self.T-1]-self.pos[0])
-
-	def update_target_force_pastor(self):
-		for t in range(0,self.T):
-			self.target_forces[t,:]=self.calc_target_force_pastor(t)
+			self.target_forces[t,:]=self.calc_target_force_auke(t)
 
 	def update_phi(self):
-
+		self.phi[:,:,:] = 0
 		for i in range(0,self.number_kernels):
-		    for t in range(0,self.T):
-		        det = 0
-		        for j in range(0,20):		        	
-		        	det += self.basis(j,t)
-		        self.phi[i,t] = self.basis(i,t)*self.calc_phase(t)/det		
+			for t in range(0,self.T):
+				self.phi[i,t,t] = self.basis(i,t)			
+
+	# def update_phi(self):
+
+	# 	for i in range(0,self.number_kernels):
+	# 	    for t in range(0,self.T):
+	# 	        det = 0
+	# 	        for j in range(0,20):		        	
+	# 	        	det += self.basis(j,t)
+	# 	        self.phi[i,t] = self.basis(i,t)*self.calc_phase(t)/det		
+
+	def update_eta(self):
+		self.eta[:,:] = 0
+		for t in range(self.T):
+			self.eta[t,:] = self.calc_phase(t)*(self.pos[self.T-1]-self.pos[0])
 
 	def learn_DMP(self):            
-		self.update_target_force_vanilla()
+		self.update_target_force_auke()
 		self.update_phi()
-		# print(self.phi)
-		self.weights = npy.dot(npy.linalg.pinv(self.phi),self.target_forces) 					
-		# print(self.weights)
+		self.update_eta()
+
+		for j in range(0,self.dim):
+			for i in range(0,self.number_kernels):
+				self.weights[i,j] = npy.transpose(self.eta[:,j])*(self.phi[i]*self.target_forces[:,j])
+				self.weights[i,j] /= npy.transpose(self.eta[:,j])*(self.phi[i]*self.eta[:,j])	
+
+		print(self.weights)
 
 	def save_DMP_parameters(self,file_suffix):
 
