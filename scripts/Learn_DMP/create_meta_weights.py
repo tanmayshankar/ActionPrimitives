27914471f512b5d08b6200c176053c9565537634
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-05 13:04:23 -0500

    Adding meta rollout points.

--- scripts/Learn_DMP/create_meta_weights.py
+++ scripts/Learn_DMP/create_meta_weights.py
@@ -12,14 +12,19 @@ segment_length = 100
 
 meta_weights = npy.zeros((number_samples,number_kernels,number_dimensions))
 meta_points = npy.zeros((number_samples,segment_length,number_dimensions))
+meta_rollout = npy.zeros((number_samples,segment_length,number_dimensions))
 
 for i in range(number_trajectories):
 	for j in range(number_segments):
 		meta_weights[number_segments*i+j] = npy.load("Traj_{0}/force_weights_{1}.npy".format(i,j))
 		meta_points[number_segments*i+j] = npy.load("Traj_{0}/position_{1}.npy".format(i,j))
+		meta_rollout[number_segments*i+j] = npy.load("Traj_{0}/roll_pos_{1}.npy".format(i,j))		
 
 with file("meta_weight_file.npy",'w') as outfile:
 	npy.save(outfile,meta_weights)
 
 with file("meta_point_file.npy",'w') as outfile:
 	npy.save(outfile,meta_points)
+
+with file("meta_rollout_points.npy",'w') as outfile:
+	npy.save(outfile,meta_rollout)
