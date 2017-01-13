b527fdcdf5116b8c61887e82e07dcb72b5ffc067
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-13 16:48:02 -0500

    Adding Cornell Data manipulation scripts.

---
+++ scripts/Cornell_Data/Parse_Skeletons.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from headers import *
+
+file_list = glob.glob("Subject1_annotations/*/*[0-9][0-9].txt")
+
+for i in range(len(file_list)):
+	# npy.genfromtxt("file.txt",delimiter=',',dtype=float,skip_footer=1)
+	skeleton = npy.genfromtxt(file_list[i],delimiter=',',dtype=float,skip_footer=1)
+
+	right_hand = skeleton[:,159:163] #Till 162, saving confidence values.
+	left_hand = skeleton[:,155:159]	#Till 158, saving confidence values.
+
+	with file("Right_Hand_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,right_hand)
+
+	with file("Left_Hand_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,left_hand)
+
+with file("File_List.npy",'w') as outfile:
+	npy.save(outfile,npy.array(file_list))
---
+++ scripts/Cornell_Data/headers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import numpy as npy
+import matplotlib.pyplot as plt
+import sys
+import random
+from scipy import signal
+import copy
+import os
+from sklearn.cluster import KMeans
+import shutil
+import subprocess
+import glob
+
