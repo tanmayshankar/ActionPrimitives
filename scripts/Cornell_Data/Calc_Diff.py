67d29a93408fc465570a6c545c4d6c979352ab94
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-13 18:14:34 -0500

    UPdating scripts.

---
+++ scripts/Cornell_Data/Calc_Diff.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from headers import *
+
+num_files = 31
+
+for i in range(num_files):
+
+	left_hand = npy.load("Traj_{0}/Original_Left_Hand_{0}.npy".format(i))
+	right_hand = npy.load("Traj_{0}/Original_Right_Hand_{0}.npy".format(i))	
+
+	left_hand_vel = npy.diff(left_hand,axis=0)
+	left_hand_acc = npy.diff(left_hand,axis=0,n=2)
+
+	time_steps = left_hand_acc.shape[0]
+
+	right_hand_vel = npy.diff(right_hand,axis=0)
+	right_hand_acc = npy.diff(right_hand,axis=0,n=2)
+
+	left_hand_vel = left_hand_vel[:time_steps]
+	right_hand_vel = left_hand_vel[:time_steps]
+	left_hand_pos = left_hand_pos[:time_steps]
+	right_hand_pos = right_hand_pos[:time_steps]
+
+	with file("left_hand_pos_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,left_hand_pos)
+
+	with file("left_hand_vel_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,left_hand_vel)
+
+	with file("left_hand_acc_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,left_hand_acc)
+
+	with file("right_hand_pos_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,right_hand_pos)
+
+	with file("right_hand_vel_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,right_hand_vel)
+
+	with file("right_hand_acc_{0}.npy".format(i),'w') as outfile:
+		npy.save(outfile,right_hand_acc)
+
--- scripts/Cornell_Data/Parse_Skeletons.py
+++ scripts/Cornell_Data/Parse_Skeletons.py
@@ -10,11 +10,13 @@ for i in range(len(file_list)):
 	right_hand = skeleton[:,159:163] #Till 162, saving confidence values.
 	left_hand = skeleton[:,155:159]	#Till 158, saving confidence values.
 
-	with file("Right_Hand_{0}.npy".format(i),'w') as outfile:
+	with file("Original_Right_Hand_{0}.npy".format(i),'w') as outfile:
 		npy.save(outfile,right_hand)
 
-	with file("Left_Hand_{0}.npy".format(i),'w') as outfile:
+	with file("Original_Left_Hand_{0}.npy".format(i),'w') as outfile:
 		npy.save(outfile,left_hand)
 
 with file("File_List.npy",'w') as outfile:
 	npy.save(outfile,npy.array(file_list))
+
+	
