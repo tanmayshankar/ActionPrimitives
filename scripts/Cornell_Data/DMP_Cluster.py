7308248b595eb93561de6445e3558809bb369ac6
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-16 11:59:24 -0500

    Update Clustering Script.

--- scripts/Cornell_Data/DMP_Cluster.py
+++ scripts/Cornell_Data/DMP_Cluster.py
@@ -19,10 +19,9 @@ segment_length = 100
 number_clusters = 40
 
 lh_nb_weights = npy.load("Meta/lh_nb_meta_weights.npy")
-lh_ob_weights = npy.load("Meta/lh_ob_meta_weights.npy")
+# lh_ob_weights = npy.load("Meta/lh_ob_meta_weights.npy")
 rh_nb_weights = npy.load("Meta/rh_nb_meta_weights.npy")
-rh_ob_weights = npy.load("Meta/rh_ob_meta_weights.npy")
-
+# rh_ob_weights = npy.load("Meta/rh_ob_meta_weights.npy")
 
 # NBL
 weights = copy.deepcopy(lh_nb_weights)
@@ -62,40 +61,40 @@ with file("nbrh_cluster_centers.npy",'w') as outfile:
 with file("nbrh_embedded_weights.npy",'w') as outfile:
 	npy.save(outfile,embedded_weights)
 
-# OBL
-weights = copy.deepcopy(lh_ob_weights)
-print("OBL")
-if (npy.isnan(weights).any()):
-	print("BAD OBL")
+# # OBL
+# weights = copy.deepcopy(lh_ob_weights)
+# print("OBL")
+# if (npy.isnan(weights).any()):
+# 	print("BAD OBL")
 
-number_samples = weights.shape[0]
-kmeans = KMeans(n_clusters = number_clusters, random_state=0).fit(weights.reshape(number_samples,number_kernels*number_dimensions))
-model = skl_manifold.TSNE(n_components=2,random_state=0)
-embedded_weights = model.fit_transform(weights.reshape(number_samples,number_kernels*number_dimensions))
+# number_samples = weights.shape[0]
+# kmeans = KMeans(n_clusters = number_clusters, random_state=0).fit(weights.reshape(number_samples,number_kernels*number_dimensions))
+# model = skl_manifold.TSNE(n_components=2,random_state=0)
+# embedded_weights = model.fit_transform(weights.reshape(number_samples,number_kernels*number_dimensions))
 
-with file("oblh_clustering_labels.npy",'w') as outfile:
-	npy.save(outfile,kmeans.labels_)
-with file("oblh_cluster_centers.npy",'w') as outfile:
-	npy.save(outfile,kmeans.cluster_centers_)
-with file("oblh_embedded_weights.npy",'w') as outfile:
-	npy.save(outfile,embedded_weights)
+# with file("oblh_clustering_labels.npy",'w') as outfile:
+# 	npy.save(outfile,kmeans.labels_)
+# with file("oblh_cluster_centers.npy",'w') as outfile:
+# 	npy.save(outfile,kmeans.cluster_centers_)
+# with file("oblh_embedded_weights.npy",'w') as outfile:
+# 	npy.save(outfile,embedded_weights)
 
 
-# OBR
-weights = copy.deepcopy(rh_ob_weights)
-print("OBR")
-if (npy.isnan(weights).any()):
-	print("BAD OBR")
+# # OBR
+# weights = copy.deepcopy(rh_ob_weights)
+# print("OBR")
+# if (npy.isnan(weights).any()):
+# 	print("BAD OBR")
 
-number_samples = weights.shape[0]
-kmeans = KMeans(n_clusters = number_clusters, random_state=0).fit(weights.reshape(number_samples,number_kernels*number_dimensions))
-model = skl_manifold.TSNE(n_components=2,random_state=0)
-embedded_weights = model.fit_transform(weights.reshape(number_samples,number_kernels*number_dimensions))
+# number_samples = weights.shape[0]
+# kmeans = KMeans(n_clusters = number_clusters, random_state=0).fit(weights.reshape(number_samples,number_kernels*number_dimensions))
+# model = skl_manifold.TSNE(n_components=2,random_state=0)
+# embedded_weights = model.fit_transform(weights.reshape(number_samples,number_kernels*number_dimensions))
 
-with file("obrh_clustering_labels.npy",'w') as outfile:
-	npy.save(outfile,kmeans.labels_)
-with file("obrh_cluster_centers.npy",'w') as outfile:
-	npy.save(outfile,kmeans.cluster_centers_)
-with file("obrh_embedded_weights.npy",'w') as outfile:
-	npy.save(outfile,embedded_weights)
+# with file("obrh_clustering_labels.npy",'w') as outfile:
+# 	npy.save(outfile,kmeans.labels_)
+# with file("obrh_cluster_centers.npy",'w') as outfile:
+# 	npy.save(outfile,kmeans.cluster_centers_)
+# with file("obrh_embedded_weights.npy",'w') as outfile:
+# 	npy.save(outfile,embedded_weights)
 
