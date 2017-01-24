5780e69dd950619a86ca18a0eb092899ed1bde2e
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-23 23:53:30 -0500

    Corrected MLR mistake

--- scripts/Cornell_Data/Calc_Diff.py
+++ scripts/Cornell_Data/Calc_Diff.py
@@ -4,12 +4,13 @@ from headers import *
 num_files = 31
 
 def move_files(i):
-	shutil.move("left_hand_pos_{0}.npy".format(i),"Traj_{0}".format(i))
-	shutil.move("left_hand_vel_{0}.npy".format(i),"Traj_{0}".format(i))
-	shutil.move("left_hand_acc_{0}.npy".format(i),"Traj_{0}".format(i))
-	shutil.move("right_hand_pos_{0}.npy".format(i),"Traj_{0}".format(i))
+	# shutil.move("left_hand_pos_{0}.npy".format(i),"Traj_{0}".format(i))
+	# shutil.move("left_hand_vel_{0}.npy".format(i),"Traj_{0}".format(i))
+	# shutil.move("left_hand_acc_{0}.npy".format(i),"Traj_{0}".format(i))
+	# shutil.move("right_hand_pos_{0}.npy".format(i),"Traj_{0}".format(i))
+	os.remove("Traj_{0}/right_hand_vel_{0}.npy".format(i))
 	shutil.move("right_hand_vel_{0}.npy".format(i),"Traj_{0}".format(i))
-	shutil.move("right_hand_acc_{0}.npy".format(i),"Traj_{0}".format(i))
+	# shutil.move("right_hand_acc_{0}.npy".format(i),"Traj_{0}".format(i))
 
 for i in range(num_files):
 
@@ -25,25 +26,26 @@ for i in range(num_files):
 	right_hand_acc = npy.diff(right_hand,axis=0,n=2)
 
 	left_hand_vel = left_hand_vel[:time_steps]
-	right_hand_vel = left_hand_vel[:time_steps]
+	right_hand_vel = right_hand_vel[:time_steps]
 	left_hand_pos = left_hand[:time_steps]
 	right_hand_pos = right_hand[:time_steps]
 
-	with file("left_hand_pos_{0}.npy".format(i),'w') as outfile:
-		npy.save(outfile,left_hand_pos)
+	# with file("left_hand_pos_{0}.npy".format(i),'w') as outfile:
+	# 	npy.save(outfile,left_hand_pos)
 
-	with file("left_hand_vel_{0}.npy".format(i),'w') as outfile:
-		npy.save(outfile,left_hand_vel)
+	# with file("left_hand_vel_{0}.npy".format(i),'w') as outfile:
+	# 	npy.save(outfile,left_hand_vel)
 
-	with file("left_hand_acc_{0}.npy".format(i),'w') as outfile:
-		npy.save(outfile,left_hand_acc)
+	# with file("left_hand_acc_{0}.npy".format(i),'w') as outfile:
+	# 	npy.save(outfile,left_hand_acc)
 
-	with file("right_hand_pos_{0}.npy".format(i),'w') as outfile:
-		npy.save(outfile,right_hand_pos)
+	# with file("right_hand_pos_{0}.npy".format(i),'w') as outfile:
+	# 	npy.save(outfile,right_hand_pos)
 
 	with file("right_hand_vel_{0}.npy".format(i),'w') as outfile:
 		npy.save(outfile,right_hand_vel)
 
-	with file("right_hand_acc_{0}.npy".format(i),'w') as outfile:
-		npy.save(outfile,right_hand_acc)
+	# with file("right_hand_acc_{0}.npy".format(i),'w') as outfile:
+	# 	npy.save(outfile,right_hand_acc)
 
+	move_files(i)
