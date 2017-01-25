bc782ed9a581d74812a64268ed38a4e6e3b31c03
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-25 18:52:09 -0500

    Updating select forces script

--- scripts/Cornell_Data/Select_Force_Segments.py
+++ scripts/Cornell_Data/Select_Force_Segments.py
@@ -5,11 +5,11 @@ num_files = 31
 num_can = 30 
 
 for i in range(num_files):
-	seg_can = npy.load("Traj_{0}/Comp_Seg_Full/rh_seg_can_30.npy".format(i))	
-	traj_len = len(npy.load("Traj_{0}/Comp_Seg_Full/rh_roll_force.npy".format(i)))
+	seg_can = npy.load("Traj_{0}/Comp_Seg_Full/lh_seg_can_30.npy".format(i))	
+	traj_len = len(npy.load("Traj_{0}/Comp_Seg_Full/lh_roll_force.npy".format(i)))
 
 	window = int(traj_len*0.08)
-	print("INDEX: _____________________", i)
+	# print("INDEX: _____________________", i)
 	# print("Window:",window)	
 	seg_indices = npy.array(seg_can[0])
 	seg_indices = seg_indices.reshape(1,-1)
@@ -22,14 +22,18 @@ for i in range(num_files):
 			seg_indices = npy.append(seg_indices,seg_can[k])		
 	
 	seg_indices = npy.sort(seg_indices)
-	seg_indices = npy.append(0,seg_indices)
+
+	if not(seg_indices[0]==0):
+		seg_indices = npy.append(0,seg_indices)
+
 	seg_indices = npy.append(seg_indices,traj_len)
 	# print("Candidates:",seg_can)
 	# print("Sorted Candidates:",npy.sort(seg_can))
 	print("FINAL INDICES:",seg_indices)
 	
-	with file("rh_seg_ind_30.npy",'w') as outfile:		
+	with file("lh_seg_ind_30.npy",'w') as outfile:		
 		npy.save(outfile,seg_indices)
 
-	shutil.move("rh_seg_ind_30.npy","Traj_{0}/Comp_Seg_Full/rh_seg_ind_30.npy".format(i))
+	shutil.move("lh_seg_ind_30.npy","Traj_{0}/Comp_Seg_Full/lh_seg_ind_30.npy".format(i))
+
 
