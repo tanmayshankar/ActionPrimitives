2d30e972de74263b185c903d0210e4c593c116ca
tanmayshankar <tanmay.shankar@gmail.com>
2017-01-24 23:50:17 -0500

    Update

--- scripts/Cornell_Data/Build_Trajectory_Library.py
+++ scripts/Cornell_Data/Build_Trajectory_Library.py
@@ -3,7 +3,7 @@ from headers import *
 
 number_trajectories = 31
 
-def move_files(i,j):	
+def move_files(i):	
 	shutil.move("force_weights.npy","Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/Comp_Segment/lh_force_weights.npy".format(i))	
 	shutil.move("roll_pos.npy","Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/Comp_Segment/lh_roll_pos.npy".format(i))
 	shutil.move("roll_vel.npy","Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/Comp_Segment/lh_roll_vel.npy".format(i))	
@@ -16,9 +16,9 @@ for i in range(0,number_trajectories):
 	print("The Counter is LEFT:",i)
 	command = "./scripts/Cornell_Data/DMP_Segment.py Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/lh_comp_pos_{0}.npy Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/lh_comp_vel_{0}.npy Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/lh_comp_acc_{0}.npy".format(i)
 	subprocess.call(command.split(),shell=False)
-	move_files(i,j)
+	move_files(i)
 
-def move_files(i,j):	
+def move_files(i):	
 	shutil.move("force_weights.npy","Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/Comp_Segment/rh_force_weights.npy".format(i))	
 	shutil.move("roll_pos.npy","Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/Comp_Segment/rh_roll_pos.npy".format(i))
 	shutil.move("roll_vel.npy","Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/Comp_Segment/rh_roll_vel.npy".format(i))	
@@ -31,5 +31,5 @@ for i in range(0,number_trajectories):
 	print("The Counter is RIGHT:",i)
 	command = "./scripts/Cornell_Data/DMP_Segment.py Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/rh_comp_pos_{0}.npy Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/rh_comp_vel_{0}.npy Data/Cornell_Data/Primitive_Library/Subject1/Traj_{0}/rh_comp_acc_{0}.npy".format(i)
 	subprocess.call(command.split(),shell=False)
-	move_files(i,j)
+	move_files(i)
 
